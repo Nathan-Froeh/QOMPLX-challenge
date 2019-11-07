e684c999d41df1a62b385f4531f65156e13cbcda
Nathan <nathan.froeh@gmail.com>
2019-11-06 17:55:30 -0700

    Add dropdown options

--- src/components/Calculator/Calculator.jsx
+++ src/components/Calculator/Calculator.jsx
@@ -9,6 +9,13 @@ class Calculator extends Component {
         <input type="text" placeholder='Total Bill'/>
         <select type="text">
           <option value=''>Choose a tip percent</option>
+          <option value={0}>0%</option>
+          <option value={5}>5%</option>
+          <option value={10}>10%</option>
+          <option value={15}>15%</option>
+          <option value={20}>20%</option>
+          <option value={25}>25%</option>
+          <option value={30}>30%</option>
         </select>
         <input type="number" placeholder='Party size'/>
         <input type="submit" value='Calc tip'/>
