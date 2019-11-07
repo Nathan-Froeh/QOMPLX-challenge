3d438b24e25a5afd2cddc92cb8faaea6fae40db0
Nathan <nathan.froeh@gmail.com>
2019-11-06 18:23:45 -0700

    Set spacing

--- src/components/Calculator/Calculator.jsx
+++ src/components/Calculator/Calculator.jsx
@@ -7,8 +7,8 @@ class Calculator extends Component {
     return (
       <div className='calculator'>
         <form>
-          <input type="text" placeholder='Total Bill'/>
-          <select type="text">
+          <input type="number" placeholder='Total Bill'/>
+          <select>
             <option value=''>Choose a tip percent</option>
             <option value={0}>0%</option>
             <option value={5}>5%</option>
@@ -21,6 +21,16 @@ class Calculator extends Component {
           <input type="number" placeholder='Party size'/>
           <input type="submit" className='submit' value='Calc tip'/>
         </form>
+        <section className='output'>
+          <div>
+            <label htmlFor="tip">Tip total:</label>
+            <p className='tip'>$Tip</p>
+          </div>
+          <div>
+            <label htmlFor="total">Tip total:</label>
+            <p className='total'>$Total</p>
+          </div>
+        </section>
       </div>
     )
   }
