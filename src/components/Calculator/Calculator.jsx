d345061bd66ddfed3c3574ead275d1574fe6dbcf
Nathan <nathan.froeh@gmail.com>
2019-11-06 18:10:29 -0700

    Section off calculator form

--- src/components/Calculator/Calculator.jsx
+++ src/components/Calculator/Calculator.jsx
@@ -5,21 +5,23 @@ class Calculator extends Component {
 
   render() {
     return (
-      <form className='calculator'>
-        <input type="text" placeholder='Total Bill'/>
-        <select type="text">
-          <option value=''>Choose a tip percent</option>
-          <option value={0}>0%</option>
-          <option value={5}>5%</option>
-          <option value={10}>10%</option>
-          <option value={15}>15%</option>
-          <option value={20}>20%</option>
-          <option value={25}>25%</option>
-          <option value={30}>30%</option>
-        </select>
-        <input type="number" placeholder='Party size'/>
-        <input type="submit" value='Calc tip'/>
-      </form>
+      <div className='calculator'>
+        <form>
+          <input type="text" placeholder='Total Bill'/>
+          <select type="text">
+            <option value=''>Choose a tip percent</option>
+            <option value={0}>0%</option>
+            <option value={5}>5%</option>
+            <option value={10}>10%</option>
+            <option value={15}>15%</option>
+            <option value={20}>20%</option>
+            <option value={25}>25%</option>
+            <option value={30}>30%</option>
+          </select>
+          <input type="number" placeholder='Party size'/>
+          <input type="submit" className='submit' value='Calc tip'/>
+        </form>
+      </div>
     )
   }
 }
